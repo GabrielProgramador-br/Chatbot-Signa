5289c0b47204ef967fbf0d7cb8e9dcb7730ec479
GabrielProgramador-br <gabrielferreira192000@gmail.com>
2025-07-03 11:34:36 -0300

    Create API_embeddings_similaridade.py
    Este script implementa uma API que recebe um texto de entrada, gera seu embedding e compara com uma base vetorial existente para retornar os itens mais semanticamente similares.

---
+++ embeddings-e- similaridade/API_embeddings_similaridade.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import pickle
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
+with open("./embeddings.pt.pkl", "rb") as f:
+    perguntas, respostas, embeddings = pickle.load(f)
+
+app = FastAPI()
+
+class Pergunta(BaseModel):
+    question: str
+
+@app.post("/pergunta_embeddings")
+async def pergunta(data: Pergunta):
+    pergunta_usuario = data.question
+    vetor_usuario = model.encode([pergunta_usuario])
+    similaridades = cosine_similarity(vetor_usuario, embeddings)
+    idx = np.argmax(similaridades)
+    score = similaridades[0][idx]
+
+    if score >= 0.65:
+        resposta = respostas[idx]
+    else:
+        resposta = "Desculpe, não encontrei uma resposta ideal. Pode reformular sua pergunta?"
+
+    return {"resposta": resposta, "score": float(score)}
